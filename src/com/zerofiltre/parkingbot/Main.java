7792fa1f92e0edde533584706087c05ec1d3f8a2
Zeleif <alyomeguja@gmail.com>
2024-02-05 18:11:47 +0100

    p1c2, ok

--- src/com/zerofiltre/parkingbot/Main.java
+++ src/com/zerofiltre/parkingbot/Main.java
@@ -7,7 +7,16 @@ public class Main {
        int length = 180;
        int width = 120;
        int surface = length * width;
+       int perimeter = (length + width) * 2;
+       int difference = length - width;
+       float ratio =(float) width / length;
        System.out.println("La surface au sol de la voiture est de: "+ surface);
+       System.out.println("le périmètre de la voiture est de: " + perimeter);
+       System.out.println("la différence entre la longueur et la largeur est de: " + difference);
+       System.out.println("le ratio entre la longueur et la largeur est de: " + ratio);
+
+
+
 
     }
 }
